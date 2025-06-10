0e0fd39d0bfcadef1bd210779560b89b88dac6b4
ziadx1999@gmail.com <ziadx1999@gmail.com>
2025-06-10 14:01:46 +0300

    create CustomNoteItem, and get packages fontawesome & fontFamily Poppins

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -11,10 +11,9 @@ class NotesViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: const [
-           SizedBox(
-            height: 50,
-          ),
+          SizedBox(height: 50),
           CustomAppBar(),
+          CustomNoteItem(),
         ],
       ),
     );
