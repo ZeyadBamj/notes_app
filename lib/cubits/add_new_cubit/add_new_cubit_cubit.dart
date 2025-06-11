7d594c03d9432be512c79addf8df9301e29c8846
ziadx1999@gmail.com <ziadx1999@gmail.com>
2025-06-11 21:54:12 +0300

    add states to add_note_state

--- lib/cubits/add_new_cubit/add_new_cubit_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'add_new_cubit_state.dart';
-
-class AddNewCubitCubit extends Cubit<AddNewCubitState> {
-  AddNewCubitCubit() : super(AddNewCubitInitial());
-}
--- lib/cubits/add_new_cubit/add_new_cubit_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'add_new_cubit_cubit.dart';
-
-@immutable
-sealed class AddNewCubitState {}
-
-final class AddNewCubitInitial extends AddNewCubitState {}
