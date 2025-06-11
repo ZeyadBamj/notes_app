b3c7503356e647cc47c5fbd5c5b679eb26483d23
ziadx1999@gmail.com <ziadx1999@gmail.com>
2025-06-11 09:56:19 +0300

    add shape to showBottomSheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -12,6 +12,9 @@ class NotesView extends StatelessWidget {
         backgroundColor: Colors.cyan,
         onPressed: () {
           showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
             context: context,
             builder: (context) {
               return AddNoteBottomSheet();
