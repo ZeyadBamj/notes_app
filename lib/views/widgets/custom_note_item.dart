ba98433b5f8283319017d2fa1fcec89334530807
ziadx1999@gmail.com <ziadx1999@gmail.com>
2025-06-12 19:40:16 +0300

    delete notes from custom_note_item.dart

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -47,7 +47,9 @@ class CustomNoteItem extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    noteModel.delete();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     color: Colors.black,
