fb7b1507e02ce525bc0cf7c7f1dae7c455380bd2
ziadx1999@gmail.com <ziadx1999@gmail.com>
2025-06-12 11:16:28 +0300

    remove ModalProgressHud from add_note_bottom_sheet.dart , add blocBuilder to form, make isLoading in button add, make AbsorbPointer

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:note_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
   final Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -16,15 +17,22 @@ class CustomButton extends StatelessWidget {
           color: kPrimaryColor,
           borderRadius: BorderRadius.circular(16),
         ),
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+        child: Center(
+          child:
+              isLoading
+                  ? SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: const CircularProgressIndicator(color: Colors.black),
+                  )
+                  : const Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
         ),
       ),
     );
